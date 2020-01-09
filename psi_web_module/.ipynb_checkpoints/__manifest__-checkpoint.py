533bbaf434961a38527efb4c90f6e7a336613673
odoo-patrick <patrick@precisonline.com>
2020-01-09 21:16:44 +0000

    testing

--- psi_web_module/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ psi_web_module/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -3,20 +3,19 @@
     'name': "psi_web_module",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        SZM hemp supply website""",
 
     'description': """
-        Long description of module's purpose
+        A custom website for SZM
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Precisonline",
+    'website': "http://www.precisionline.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Website',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
