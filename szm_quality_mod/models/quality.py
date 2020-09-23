aae01ffdbf7c94df9244e600d9fa8e33848c7a04
Odoo-JeffMueller <51723739+Odoo-JeffMueller@users.noreply.github.com>
2020-09-23 15:11:14 -0600

    Added Dates to Model

--- szm_quality_mod/models/quality.py
+++ szm_quality_mod/models/quality.py
@@ -23,5 +23,8 @@ class QualityAlert(models.Model):
     szm_prod_rpt_nbr = fields.Char('Product Report#')
     szm_mtl_qty = fields.Integer('Material Affected')
     szm_disposition = fields.Html('Disposition')
+    szm_disposition_date = fields.Datetime(string='Disposition Date', required=False, index=True, help="Date Dispositon Completed")
     szm_eval_review = fields.Html('Evaluation/Review')
+    szm_eval_date = fields.Datetime(string='Evaluation Date', required=False, index=True, help="Date Evaluation Completed")
     szm_root_cause = fields.Html('Root Cause Analysis')
+    szm_rc_date = fields.Datetime(string='Root Cause Date', required=False, index=True, help="Date Root Cause Completed")
