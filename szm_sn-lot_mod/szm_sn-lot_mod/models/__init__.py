fb66049c9ba2a8327d4bedf8dc35c5ea16646980
Odoo-JeffMueller <51723739+Odoo-JeffMueller@users.noreply.github.com>
2020-10-28 08:50:34 -0600

    Base Install

---
+++ szm_sn-lot_mod/szm_sn-lot_mod/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from . import company
+from . import mrp_product_produce
+from . import mrp_production
+from . import mrp_workorder
+from . import product_template
+from . import res_config_settings
+from . import stock_production_lot
