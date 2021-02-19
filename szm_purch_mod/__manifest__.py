24d7997b2d312e1a7528f4857f17ab8da0dd2123
Odoo-JeffMueller <51723739+Odoo-JeffMueller@users.noreply.github.com>
2021-02-19 14:02:48 -0700

    Minor Changes

--- szm_purch_mod/__manifest__.py
+++ szm_purch_mod/__manifest__.py
@@ -16,7 +16,7 @@
     "license": "AGPL-3",
     'category': 'Purchasing',
     'sequence': 10,
-    'version': '13.0.2.0',
+    'version': '13.0.2.1',
     'installable': True,
     'application': False,
     'auto_install': False,
