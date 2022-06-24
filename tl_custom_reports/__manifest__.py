3eb33125acf83d926d8adc6d8a3ed8878df91ad2
Odoo-JeffMueller <51723739+Odoo-JeffMueller@users.noreply.github.com>
2022-06-24 16:38:35 -0600

    Order - BOM Structure

--- tl_custom_reports/__manifest__.py
+++ tl_custom_reports/__manifest__.py
@@ -3,20 +3,21 @@
 {
     'name': 'Custom Report Templates',
 
-    'summary': 'Customized Report Template for Quotation/SO/Sales',
+    'summary': 'Customized Report Templates',
 
     'description': """
-		    Customize report, customize pdf report, customize template report, Customize Sales Order report,
-        Customize Purchase Order report, Customize invoice report, Customize delivery Order report
+        Order/Quotation Form Change to meet Tenacious Labs Requirements
+        without impacting standard forms. Company specific form capability.
+        BOM Structure changed decimal precision for costs 
     """,
 
     'author': 'Jeff Mueller',
     'website': 'https://www.jrmsyssol.com',
 
     'category': 'Sales',
-    'version': '15.0.0.2',
+    'version': '15.0.0.1',
     'license': 'LGPL-3',
-    'depends': ['base', 'account', 'sale', 'sale_management'],
+    'depends': ['base', 'account', 'sale', 'sale_management', 'mrp'],
     'installable': True,
     'auto_install': False,
     'application': False,
@@ -27,6 +28,8 @@
         "reports/fancy_report_saleorder.xml",
         "reports/modern_report_saleorder.xml",
         "reports/odoo_standard_report_saleorder.xml",
+        "reports/report_mrp_bom.xml",
+        "reports/report_mrp_bom_line.xml",
              ],
     'demo': [],
 }
